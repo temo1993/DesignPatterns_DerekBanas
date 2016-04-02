fd87a0ef24ac5c98507b8f55135bb8944daa2098
temo <temo.kiknavelidze@gmail.com>
2016-04-02 22:15:49 +0400

    ...

--- src/Composite/SongComponent.java
+++ src/Composite/SongComponent.java
@@ -2,7 +2,7 @@ package Composite;// This acts as an interface for every Song (Leaf)
 // and SongGroup (Composite) we create
 
 abstract class SongComponent {
-	
+
 	// We throw UnsupportedOperationException so that if
 	// it doesn't make sense for a song, or song group
 	// to inherit a method they can just inherit the
@@ -11,49 +11,37 @@ abstract class SongComponent {
 	// This allows me to add components
 	
 	public void add(SongComponent newSongComponent) {
-		
 		throw new UnsupportedOperationException();
-		
 	}
 	
 	// This allows me to remove components
 	
 		public void remove(SongComponent newSongComponent) {
-			
 			throw new UnsupportedOperationException();
-			
 	}
 		
 	// This allows me to get components
 		
 	public SongComponent getComponent(int componentIndex) {
-			
 		throw new UnsupportedOperationException();
-			
 	}
 	
 	// This allows me to retrieve song names
 	
 	public String getSongName() {
-			
 		throw new UnsupportedOperationException();
-			
 	}
 	
 	// This allows me to retrieve band names
 	
 	public String getBandName() {
-				
 		throw new UnsupportedOperationException();
-				
 	}
 	
 	// This allows me to retrieve release year
 	
 	public int getReleaseYear() {
-				
 		throw new UnsupportedOperationException();
-				
 	}
 	
 	// When this is called by a class object that extends
@@ -61,9 +49,7 @@ abstract class SongComponent {
 	// specific to the Song or SongGroup
 	
 	public void displaySongInfo() {
-				
 		throw new UnsupportedOperationException();
-				
 	}
 	
 }
